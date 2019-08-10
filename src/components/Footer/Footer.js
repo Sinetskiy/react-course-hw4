7c95ed8f496fe7d9356c916955158b6670b4a1a9
Andrey Sinetskiy <sinetskiy@gmail.com>
2019-08-10 21:15:04 +0300

    hw4

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,17 @@
-import React, { PureComponent } from 'react';
-import { AuthConsumer } from '../../contexts/Auth';
+import React, {PureComponent} from 'react';
+import {AuthConsumer} from '../../contexts/Auth';
 import './Footer.css';
 
 class Footer extends PureComponent {
-  render() {
-    return 'empty';
-  }
+    render() {
+        return <AuthConsumer>
+            {({isAuthorized, email}) => {
+                return <div>
+                    <p className="t-footer">{isAuthorized ? `Вы вошли как ${email}` : 'Вы гость в этой системе'}</p>
+                </div>;
+            }}
+        </AuthConsumer>
+    }
 }
 
 export default Footer;
